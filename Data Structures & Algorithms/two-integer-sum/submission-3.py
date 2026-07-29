f8ed6104268f9b80272682a51bb8a3d109b8a151
Kyle Kent <56409246+Kladenets@users.noreply.github.com>
2026-07-29 16:45:26 -0400

    Add: two-integer-sum - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum/submission-3.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # we have a list of nums, and we're looking for two that add up to target
+        # which means for every num, we know what compliment were looking for: target-num
+
+        comps = {}
+        for i, n in enumerate(nums):
+            if n in comps:
+                return [comps[n], i]
+            
+            comps[target - n] = i
+
+        return [-1,-1]
